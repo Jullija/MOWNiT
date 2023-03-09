c95cd2666ecf0a3948e6391c1702cd4c7bc1f01c
Julija <juleczek2002@gmail.com>
2023-03-09 16:50:50 +0100

    Lab2 on finish

---
+++ lab2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <gsl/gsl_ieee_utils.h>
+#include <gsl/gsl_math.h>
+
+int main(){
+    gsl_ieee_env_setup();
+    float num = 1.0/3.0;
+    gsl_ieee_printf_float(&num);
+    
+    int i = 0;
+    while (i < 30){
+        num = num/2;
+        gsl_ieee_printf_float(&num);
+        printf("\n");
+        i += 1;
+    }
+    return 0;
+    
+}
