9165ba3a10382e63856cf964b7c6f5e5836da6a7
Julija <juleczek2002@gmail.com>
2023-03-09 21:58:54 +0100

    Lab2 done

--- lab2/main.c
+++ lab2/main.c
@@ -6,14 +6,14 @@ int main(){
     gsl_ieee_env_setup();
     float num = 1.0/3.0;
     gsl_ieee_printf_float(&num);
+    printf("\n");
     
     int i = 0;
-    while (i < 30){
+    while (i < 150){
         num = num/2;
         gsl_ieee_printf_float(&num);
         printf("\n");
         i += 1;
     }
     return 0;
-    
 }
